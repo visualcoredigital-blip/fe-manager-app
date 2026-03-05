08c64d3d3a108dcceb95d2b7c9ddfb153de18df6
fchacon <fchacon@bnamericas.com>
2026-03-05 14:06:22 -0300

    configura variables

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
+// Usamos import.meta.env para leer del .env en local y de Vercel en la nube
 export const authApi = axios.create({
-    baseURL: 'http://localhost:8080/api/contacts/'
+    baseURL: import.meta.env.VITE_AUTH_API_URL 
 });
 
 export const managerApi = axios.create({
-    baseURL: 'http://localhost:8080/api/contacts'
+    baseURL: import.meta.env.VITE_MANAGER_API_URL
 });
 
 // Interceptor para añadir el token automáticamente al Manager
